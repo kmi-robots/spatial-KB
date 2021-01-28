8b7a8bb5a931e459f2997b50663c92b5971e1264
achiatti <agnese.chiatti@gmail.com>
2021-01-28 09:35:35 +0000

    explicit geosparql load

--- onto.py
+++ onto.py
@@ -5,7 +5,7 @@ import types
 
 def init_onto(IRI) -> Ontology:
     # Import base spatial ontology from IRI
-    onto = get_ontology(IRI).load()
-    with onto:
-        for cl in onto.classes(): print(cl)
+    # Load geosparql from local first (fixed DublinCore link)
+    geosparql = get_ontology('./data/ont/geosparql_vocab_all.rdf').load()
+    onto = get_ontology(IRI).load() # this owl extens geosparql so it is based on the local fixed version now
     return onto
